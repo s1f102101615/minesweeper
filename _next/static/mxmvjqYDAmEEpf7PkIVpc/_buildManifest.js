48f18245da15dc0f20ae22f8ed419834df4b1f57
s1f102101615 <s1f102101615@users.noreply.github.com>
2023-05-22 17:13:47 +0000

    deploy: 885de5eacbc7d71ed12fde1a117d7394a55562b6

--- _next/static/mxmvjqYDAmEEpf7PkIVpc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2084a231171e9b48.css","static/chunks/pages/index-fedf9ae4ced1a23d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
