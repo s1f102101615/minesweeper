aa80352a6840d8f9df8bdb4a99efb4c68f60c758
s1f102101615 <s1f102101615@iniad.org>
2023-05-23 00:43:06 +0900

    マインスイーパー部分ほぼ完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,75 +48,92 @@ const Home = () => {
     [-1, 0],
   ];
 
-  const bombrize = () => {
+  const bombrize = (one: (0 | 1 | 2 | 3)[][]) => {
     let bombCount = 0;
+    const newBombMap = [...bombMap]; // 新しい爆弾の配置を保持する配列
+
     while (bombCount < 10) {
       const randomRow = Math.floor(Math.random() * 9);
       const randomCol = Math.floor(Math.random() * 9);
-      if (userInputs[randomRow][randomCol] !== 1) {
-        setBombMap((prevBombMap) => {
-          const newBombMap = [...prevBombMap];
-          newBombMap[randomRow][randomCol] = 1;
-          return newBombMap;
-        });
+
+      if (one[randomRow][randomCol] !== 1 && newBombMap[randomRow][randomCol] !== 1) {
+        newBombMap[randomRow][randomCol] = 1;
         bombCount++;
+      }
+    }
+
+    setBombMap(newBombMap); // 新しい爆弾の配置をセット
+  };
+
+  const void_chain = (t: number, n: number) => {
+    let bombnum = 0;
+    if (bombMap[t][n] !== 1) {
+      for (const d of directions) {
+        const k = t + d[0];
+        const p = n + d[1];
+        if (bombMap[t + d[0]] === undefined || bombMap[t + d[0]][n + d[1]] === undefined) {
+          continue;
+        } else if (bombMap[k][p] === 1) {
+          bombnum++;
+        }
+      }
+      if (bombnum === 0) {
+        board[t][n] = 0;
+        for (const d of directions) {
+          if (
+            board[t + d[0]] === undefined ||
+            board[t + d[0]][n + d[1]] === undefined ||
+            board[t + d[0]][n + d[1]] === 0
+          ) {
+            continue;
+          }
+          board[t + d[0]][n + d[1]] = 0;
+          void_chain(t + d[0], n + d[1]);
+        }
+      }
+      if (bombnum !== 0) {
+        board[t][n] = bombnum;
+      }
+    } else {
+      for (let p = 0; p < 9; p++) {
+        for (let k = 0; k < 9; k++) {
+          if (bombMap[p][k] === 1) {
+            board[p][k] = 11;
+          }
+        }
       }
     }
   };
 
+  //空白分裂
   for (let t = 0; t < 9; t++) {
     for (let n = 0; n < 9; n++) {
       if (userInputs[t][n] === 1) {
-        let bombnum = 0;
-        for (const d of directions) {
-          if (bombMap[t + d[0]] === undefined || bombMap[t + d[0]][n + d[1]] === undefined) {
-            continue;
-          } else if (bombMap[t + d[0]][n + d[1]] === 1) {
-            bombnum++;
-          }
-        }
-        board[t][n] = bombnum;
-        console.table(board);
-        //ばくだん
-        if (bombMap[t][n] === 1) {
-          board[t][n] = 11;
-        }
+        void_chain(t, n);
       }
     }
   }
-
-  //空白分裂
+  console.table(board);
 
   const onClick = (x: number, y: number) => {
     const newuserInputs: (0 | 1 | 2 | 3)[][] = userInputs.map((row) =>
       row.map((cell) => cell as 0 | 1 | 2 | 3)
     );
     const newbombMap: number[][] = JSON.parse(JSON.stringify(bombMap));
+
     if (bombMap[y][x] === 1) {
       alert('爆発！');
     }
     newuserInputs[y][x] = 1;
-    console.table(bombMap);
-    console.log('aaaaaa');
-    setUserInputs(newuserInputs);
 
     // ここは爆弾を置く if
     const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
     if (!isPlaying) {
-      bombrize();
-      // for (let t = 0; t < 9; t++) {
-      //   for (let n = 0; n < 9; t++) {
-      //     let bombnum = 0;
-      //     for (const d of directions) {
-      //       if (bombMap[t + d[0]] !== undefined && bombMap[t + d[0]][n + d[1]] !== undefined) {
-      //         continue;
-      //       }
-      //       bombnum++;
-      //     }
-      //     board[t][n] = 1;
-      //   }
-      // }
+      bombrize(newuserInputs);
     }
+    console.table(bombMap);
+    console.log('aaaaaa');
+    setUserInputs(newuserInputs);
   };
 
   return (
@@ -128,9 +145,11 @@ const Home = () => {
               <div
                 className={styles.cell}
                 style={{
-                  opacity: color === 0 ? 0 : 1,
-                  backgroundImage: color !== -1 ? 'url()' : '',
-                  border: color !== -1 ? '' : '',
+                  borderTopColor: color === -1 ? 'rgb(255 255 255)' : 'rgb(74 74 74)',
+                  borderRightColor: color === -1 ? 'rgb(134 134 134)' : 'rgb(74 74 74)',
+                  borderBottomColor: color === -1 ? 'rgb(134 134 134)' : 'rgb(74 74 74)',
+                  borderLeftColor: color === -1 ? 'rgb(255 255 255)' : 'rgb(74 74 74)',
+                  borderWidth: color === -1 ? '4px' : '0.5px',
                 }}
                 key={`${x}-${y}`}
                 onClick={() => onClick(x, y)}
