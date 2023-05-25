155f144b329aef4d63b0902f23e4289a30b3b11a
s1f102101615 <s1f102101615@users.noreply.github.com>
2023-05-25 11:54:30 +0000

    deploy: 478f1851c813e2a36682a7716bb1aba88c380f59

--- _next/static/e4Y5eLviT-T_yrm5ff-ib/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/31831af8ec003349.css","static/chunks/pages/index-90f14923eedc2b37.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/lafhWtECCla4hPrbbGnOS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ba7ea5e4f2c88911.css","static/chunks/pages/index-4c0f2f1542ff25a1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
